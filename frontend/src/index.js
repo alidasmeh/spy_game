d637356be8a2776420fccaede3e261dbabf40d35
Ali Dasmeh <dasmeh@mpib-berlin.mpg.de>
2024-11-08 12:16:35 +0100

    minor update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,14 +12,6 @@ import RegistrationPage from './pages/Registration';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Navigation links */}
-    {/* <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-    </nav> */}
-
     <BrowserRouter>
       <Routes>
         <Route path="/">
