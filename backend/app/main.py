cd0d7aa4228572c742492bc366aa01d7f933913d
Ali Dasmeh <dasmeh@mpib-berlin.mpg.de>
2024-11-08 17:02:07 +0100

    clean up

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,23 +20,3 @@ app.add_middleware(
 
 app.include_router(api, prefix="/api")
 app.mount("/ss", app=sio_app)
-
-
-#     query = "SELECT * FROM players"
-#     items = await conn.fetch(query)
-#     return items
-# ----------
-#     try:
-#         inserted_id = await conn.fetchval('INSERT INTO players (username, email) VALUES($1, $2) RETURNING id', username, email)
-
-#     except asyncpg.exceptions.UniqueViolationError as e:
-#         raise HTTPException(
-#             status_code=status.HTTP_400_BAD_REQUEST,
-#             detail=f"Error: A unique constraint violation occurred: {e}"
-#         )
-    
-#     except Exception as e:
-#         raise HTTPException(
-#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#             detail=f"Error: not defined error : {e}"
-#         )
